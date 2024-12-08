fb2bb77f26cd8e3964a4466548ffabaeb14a6a00
GalaxyVerse <133721816+GalaxyVerseV2@users.noreply.github.com>
2024-12-07 20:36:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+ <script>
+        document.getElementById('feedbackButton').onclick = function() {
+            const feedback = prompt("Enter your username and then your feedback (EX: Nate123 - Add more games)");
+            if (feedback) {
+                sendFeedback(feedback);
+            } else {
+                alert("Feedback cannot be empty.");
+            }
+        };
 
+        function sendFeedback(feedback) {
+            const webhookUrl = 'https://discord.com/api/webhooks/1310025772588732487/NBeW92ka5y-3tF851T4CplINNtDczQpPIu1Q9bmC-AudM0WEoM_ApcE7e6nSYdch2vO1'; // Replace with your actual webhook URL
+            const payload = {
+                content:  ` ||****Feedback Log:****|| ${feedback}` // Format the message for Discord
+            };
+
+            fetch(webhookUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(payload)
+            })
+            .then(response => {
+                if (response.ok) {
+                    alert("Feedback request has been sent. Thank you");
+                } else {
+                    alert("There was an error sending your feedback. Please try again.");
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert("There was an error sending your feedback. Please try again.");
+            });
+        }
+
+        // Check for saved theme in localStorage
+        const currentTheme = localStorage.getItem('theme');
+        if (currentTheme === 'dark') {
+            document.body.classList.add('dark-mode');
+            document.querySelector('.navbar').classList.add('dark-mode');
+        }
+    </script>
+
+</body>
+</html>
